0b6235cbc804760d57f6259c5e4dc32212931c97
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-06-19 23:26:40 +0100

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-
-print(settings.SECRET_KEY)
-print(os.environ.get('SECRET_KEY')) 
-print(os.environ.get('EMAIL_HOST_PASSWORD'))
