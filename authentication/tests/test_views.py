b00d4b1dc7f8723406589091ae759263b7b55d78
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-06-20 20:35:58 +0100

    test circleci config

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -7,7 +7,7 @@ class TestViews(TestSetup):
 
     def test_user_cannot_register_with_no_data(self):
         res=self.client.post(self.register_url)
-        self.assertEqual(res.status_code, 400)
+        self.assertEqual(res.status_code, 200)
 
     def test_user_can_register_correctly(self):
         res=self.client.post(self.register_url, self.user_data, format="json")
